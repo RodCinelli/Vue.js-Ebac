530365637bfc2cc3e71b59ef0f65773feddbbabe
Rodrigo Cinelli <rodcinelli@gmail.com>
2024-06-05 21:07:10 -0300

    Aplicando estilo de forma condicional

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { reactive } from 'vue';
+
 const nome = 'Rodrigo Cinelli'
 const meuObj = {
   nome: 'Rodrigo Cinelli',
@@ -18,18 +20,75 @@ const gostaDaGoogle = false;
 const gostaDoBing = false;
 
 const estaAutorizado = false;
+
+// let contador = 0;
+const estado = reactive({
+  contador: 0,
+  email: '',
+  saldo: 5000,
+  transferindo: 0
+});
+
+function incrementar() {
+  estado.contador++;
+}
+
+function decrementar() {
+  estado.contador--;
+}
+
+function alteraEmail(event) {
+  estado.email = event.target.value;
+}
+
+function mostraSaldoFuturo() {
+  const { saldo, transferindo } = estado;
+  return saldo - transferindo;
+}
+
+function validaValorTransferencia() {
+  const { saldo, transferindo } = estado;
+  return saldo >= transferindo;
+}
+
 </script>
 
 <template>
   <h1>{{ dizOla('paula') }}</h1>
-  <img v-if="gostaDaGoogle":src="imagemDoGoogle" alt="">
-  <img v-else-if="gostaDoBing":src="imagemDoBing" alt="">
+  <img v-if="gostaDaGoogle" :src="imagemDoGoogle" alt="">
+  <img v-else-if="gostaDoBing" :src="imagemDoBing" alt="">
   <h2 v-else>Não curte o Google e o Bing </h2>
 
   <h1 v-if="estaAutorizado">Bem-vindo</h1>
   <h1 v-else>Não possui acesso</h1>
 
   <button :disabled="botaoEstaDesabilitado">enviar mensagem</button>
+
+  <br />
+  <hr />
+
+  {{ estado.contador }}
+
+  <button @click="incrementar" type="button">+</button>
+  <button @click="decrementar" type="button">-</button>
+
+  <br />
+  <hr />
+
+  {{ estado.email }}
+
+  <input type="email" @keyup="alteraEmail">
+
+  <br />
+  <hr />
+
+  Saldo: {{ estado.saldo }} <br />
+  Trasferindo: {{ estado.transferindo }} <br />
+  Saldo depois da transferência: {{ mostraSaldoFuturo() }} <br />
+  <input class="campo" :class="{ invalido: !validaValorTransferencia() }" @keyup="evento => estado.transferindo = evento.target.value" type="number" placeholder="Quantia para transferir" />
+  <button v-if="validaValorTransferencia()">Transferir</button>
+  <span v-else>Valor maior que o saldo</span>
+
 </template>
 
 <style scoped>
@@ -37,4 +96,14 @@ const estaAutorizado = false;
 img {
   max-width: 200px;
 }
+
+.invalido {
+  outline-color: red;
+  border-color: red;
+}
+
+.campo {
+  border: 2px solid #000;
+}
+
 </style>
